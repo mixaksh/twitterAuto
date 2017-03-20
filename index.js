f2648d5757996fa67d771c0d1e8463a0843df9e3
Milutin Spasic <Milutin Spasic>
2017-03-20 10:10:08 +0100

    listening port in index

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var T = new Twit({
   consumer_secret: "Vf1SYn6CbAjEgDNfqFhb4YdxU53nKQXznxJxsB7FmH196Dax97",
   access_token: "842392197827842048-DEijc8zzi79C9hTOcD8gnUIdgo50AGP",
   access_token_secret: "5p9bZSgvAhEyIejATF4vOClyCmdAgAvaDbgga0Pe7R6lV"
-});
+}).listen(process.env.PORT);
 
 module.exports = T;
